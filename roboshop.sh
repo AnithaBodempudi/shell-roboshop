948b6a9185272caa989675b20147285560ebc0bb
AnithaBodempudi <anithabodempudi125@gmail.com>
2025-06-24 14:32:30 -0500

    hellow world started

--- roboshop.sh
+++ roboshop.sh
@@ -1,22 +1,63 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-073029036616b0bac"
-INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "catalogue" "user" "cart" 
-"shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z064726138Q1E4WUQ2NR0"
-DOMAIN_NAME="devsecopspractice.site"
+#!/bin/bash
 
-for instance in ${INSTANCES[@]}
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-073029036616b0bac" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z064726138Q1E4WUQ2NR0" # replace with your ZONE ID
+DOMAIN_NAME="devsecopspractice.site" # replace with your domain
+
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
- INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-073029036616b0bac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-073029036616b0bac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-       
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
-       
+        RECORD_NAME="$DOMAIN_NAME"
     fi
-    echo "$instance Ip address: $IP"
+    echo "$instance IP address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
 done
+
+# AMI_ID="ami-09c813fb71547fc4f"
+# SG_ID="sg-073029036616b0bac"
+# INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "catalogue" "user" "cart" 
+# "shipping" "payment" "dispatch" "frontend")
+# ZONE_ID="Z064726138Q1E4WUQ2NR0"
+# DOMAIN_NAME="devsecopspractice.site"
+
+# for instance in ${INSTANCES[@]}
+# do
+#  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-073029036616b0bac --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+#     if [ $instance != "frontend" ]
+#     then
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+       
+#     else
+#         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+       
+#     fi
+#     echo "$instance Ip address: $IP"
+# done
