1535c3843ff097037297cabe80f4b9fa1cd8b3eb
AnithaBodempudi <anithabodempudi125@gmail.com>
2025-07-14 16:56:44 -0500

    hellow world started

--- catalogue.sh
+++ catalogue.sh
@@ -42,32 +42,46 @@ VALIDATE $? "Enabling nodejs:20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing nodejs:20"
 
+# id roboshop
+# if [ $? -ne 0 ]
+# then
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
 VALIDATE $? "Creating roboshop system user"
+# else
+#     echo -e "System user roboshop already created ... $Y SKIPPING $N"
+# fi
 
-mkdir /app
-VALIDATE $? "creaating app directory"
+mkdir /app 
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Catalogue"
 
-cd /app
+# rm -rf /app/*
+cd /app 
 unzip /tmp/catalogue.zip &>>$LOG_FILE
 VALIDATE $? "unzipping catalogue"
 
 npm install &>>$LOG_FILE
 VALIDATE $? "Installing Dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/caatalogue.service
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Copying catalogue service"
 
 systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue &>>$LOG_FILE
+systemctl enable catalogue  &>>$LOG_FILE
 systemctl start catalogue
-VALIDATE $? "Starting catalogue"
+VALIDATE $? "Starting Catalogue"
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
+# STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+# if [ $STATUS -lt 0 ]
+# then
 mongosh --host mongodb.devsecopspractice.site </app/db/master-data.js &>>$LOG_FILE
+#     VALIDATE $? "Loading data into MongoDB"
+# else
+#     echo -e "Data is already loaded ... $Y SKIPPING $N"
+# fi
